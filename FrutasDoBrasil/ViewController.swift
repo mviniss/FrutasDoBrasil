d9fd0b267fc00dc774150b8a2ce7bc69cb3e9b6f
mviniss <m_vinicius.ss@icloud.com>
2022-06-07 16:13:46 -0300

    1st commit
    Prova de conceito utilizando o Cupuaçu

--- FrutasDoBrasil/ViewController.swift
+++ FrutasDoBrasil/ViewController.swift
@@ -7,18 +7,44 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,ARSessionDelegate{
     
     @IBOutlet var arView: ARView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let configuration = ARImageTrackingConfiguration()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        if let imagesToTrack = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: Bundle.main){
+            configuration.trackingImages = imagesToTrack
+            configuration.maximumNumberOfTrackedImages = 1
+        }
+        
+        arView.session.run(configuration)
+        arView.session.delegate = self
+    }
+    
+    func session (_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        
+        for anchor in anchors {
+            
+            guard let imageAnchor = anchor as? ARImageAnchor else {return}
+            
+            if let imageName = imageAnchor.name, imageName == "CupuacuCard" {
+                let entity = AnchorEntity(anchor: imageAnchor)
+                
+                if let scene = try? Experience.loadBox() {
+                    if let cup = scene.findEntity(named: "Cupuacu"){
+                        entity.addChild(cup)
+                        arView.scene.addAnchor(entity)
+                        
+                    }
+                }
+            }
+            
+        }
     }
 }
